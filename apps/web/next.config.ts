46a005f54a4ffd094454a498e7290593d84a9b39
shouta0715 <kshouta0715@gmail.com>
2025-04-28 21:08:11 +0900

    fix: next config

--- apps/web/next.config.ts
+++ apps/web/next.config.ts
@@ -1,4 +1,7 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {};
+import { NextConfig } from "next";
+
+const nextConfig: NextConfig = {
+  reactStrictMode: true,
+};
 
 export default nextConfig;
