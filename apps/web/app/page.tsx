4a1cf8f7bce3e287f97aea8066f95428c45fb25a
shouta0715 <kshouta0715@gmail.com>
2025-04-29 19:44:45 +0900

    add: fonts

--- apps/web/app/page.tsx
+++ apps/web/app/page.tsx
@@ -1,7 +1,8 @@
 export default function Home() {
   return (
-    <div>
-      <h1>Hello</h1>
+    <div className="text-2xl font-bold">
+      <p>Hello World</p>
+      <p>こんにちは</p>
     </div>
   );
 }
