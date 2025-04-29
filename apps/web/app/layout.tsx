f6b2fbaf8f95f817a19c94e4b673e6e62249a4c6
shouta0715 <kshouta0715@gmail.com>
2025-04-29 19:44:57 +0900

    add: fonts

--- apps/web/app/layout.tsx
+++ apps/web/app/layout.tsx
@@ -1,10 +1,25 @@
-export default function RootLayout({
-  children,
-}: Readonly<{
-  children: React.ReactNode;
-}>) {
+import "@repo/tailwind-config/global.css";
+import { GeistSans as geistSans } from "geist/font/sans";
+import { cn } from "../lib/cn";
+import { Noto_Sans_JP } from "next/font/google";
+
+const notoSans = Noto_Sans_JP({
+  subsets: ["latin"],
+  variable: "--font-noto-sans",
+  display: "swap",
+});
+
+type Props = {
+  children: Readonly<React.ReactNode>;
+};
+
+export default function RootLayout({ children }: Props) {
   return (
-    <html lang="ja">
+    <html
+      lang="ja"
+      className={cn(geistSans.variable, notoSans.variable, "font-noto-sans")}
+      suppressHydrationWarning
+    >
       <body>{children}</body>
     </html>
   );
